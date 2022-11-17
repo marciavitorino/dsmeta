5699d2002baf1373a64a44fdbbbc27417d133a08
marciavitorino <marciamaducosta@gmail.com>
2022-11-17 01:02:41 -0300

    UseState

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,5 +1,4 @@
-import logo from '../../assets/img/logo.svg'
-
+import logo from '../../assets/img/logo.svg';
 
 
 function Header() {
@@ -19,4 +18,4 @@ function Header() {
 
 }
 
-export default Header
+export default Header;

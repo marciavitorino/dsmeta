a2495f6ee2da76e7b40fdaed9ea2ce393ca932dd
marciavitorino <marciamaducosta@gmail.com>
2022-11-20 13:42:06 -0300

    Date update

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -19,11 +19,19 @@ function SalesCard() {
     const [sales, setSales] = useState<Sale[]>([]);
 
     useEffect(() => {
-        axios.get(`${BASE_URL}/sales`)
+
+        const dmin = minDate.toISOString().slice(0, 10);
+        const dmax = maxDate.toISOString().slice(0, 10);
+
+        console.log('dmin=' + dmin); 
+        console.log('dmax=' + dmax);
+
+        axios.get(`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`)
             .then(response => {
+                console.log(response.data);
                 setSales(response.data.content);
             });
-    }, []);
+    }, [minDate, maxDate]);
 
     return (
         <div className="dsmeta-card">
@@ -67,12 +75,9 @@ function SalesCard() {
                                 </tr>
                             )
                         })}
-
                     </tbody>
-
                 </table>
             </div>
-
         </div>
     )
 
